27f4a3818c6427bd73571448a43e2ef8789fe752
Agnieszka Łupińska <agnieszka.gesiewska@o2.pl>
2025-05-10 06:43:42 +0200

    add StarRating

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -18,7 +18,7 @@ const initialState = {
       isFavorite: true,
       isCompare: true,
       oldPrice: 50,
-      myReting: 4,
+      myRating: 4,
     },
     {
       id: 'aenean-ru-bristique-2',
